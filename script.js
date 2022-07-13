e263ee98f47605151732949e45fe333cc4359b41
Ashish kumar anand <ashishraj5296@gmail.com>
2022-07-13 14:51:46 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,67 @@
+function myFunction() {
+    var element = document.getElementById("first").style.display="none";
+    var element = document.getElementById("mainn").style.display="flex";
+}
+function personal() {
+   var element = document.getElementById("personal").style.display="block";
+   var element = document.getElementById("contact").style.display="none";
+   var element = document.getElementById("projects").style.display="none";
+   var element = document.getElementById("certificate").style.display="none";
+   var element = document.getElementById("skills").style.display="none";
+   var element = document.getElementById("education").style.display="none";
+   var element = document.getElementById("hobbies").style.display="none";
+}
+function contact() {
+   var element = document.getElementById("personal").style.display="none";
+   var element = document.getElementById("contact").style.display="block";
+   var element = document.getElementById("projects").style.display="none";
+   var element = document.getElementById("certificate").style.display="none";
+   var element = document.getElementById("skills").style.display="none";
+   var element = document.getElementById("education").style.display="none";
+   var element = document.getElementById("hobbies").style.display="none";
+}
+function projects() {
+   var element = document.getElementById("personal").style.display="none";
+   var element = document.getElementById("contact").style.display="none";
+   var element = document.getElementById("projects").style.display="block";
+   var element = document.getElementById("certificate").style.display="none";
+   var element = document.getElementById("skills").style.display="none";
+   var element = document.getElementById("education").style.display="none";
+   var element = document.getElementById("hobbies").style.display="none";
+}
+function certificate() {
+   var element = document.getElementById("personal").style.display="none";
+   var element = document.getElementById("contact").style.display="none";
+   var element = document.getElementById("projects").style.display="none";
+   var element = document.getElementById("certificate").style.display="block";
+   var element = document.getElementById("skills").style.display="none";
+   var element = document.getElementById("education").style.display="none";
+   var element = document.getElementById("hobbies").style.display="none";
+}
+function skills() {
+   var element = document.getElementById("personal").style.display="none";
+   var element = document.getElementById("contact").style.display="none";
+   var element = document.getElementById("projects").style.display="none";
+   var element = document.getElementById("certificate").style.display="none";
+   var element = document.getElementById("skills").style.display="block";
+   var element = document.getElementById("education").style.display="none";
+   var element = document.getElementById("hobbies").style.display="none";
+}
+function education() {
+   var element = document.getElementById("personal").style.display="none";
+   var element = document.getElementById("contact").style.display="none";
+   var element = document.getElementById("projects").style.display="none";
+   var element = document.getElementById("certificate").style.display="none";
+   var element = document.getElementById("skills").style.display="none";
+   var element = document.getElementById("education").style.display="block";
+   var element = document.getElementById("hobbies").style.display="none";
+}
+function hobbies() {
+   var element = document.getElementById("personal").style.display="none";
+   var element = document.getElementById("contact").style.display="none";
+   var element = document.getElementById("projects").style.display="none";
+   var element = document.getElementById("certificate").style.display="none";
+   var element = document.getElementById("skills").style.display="none";
+   var element = document.getElementById("education").style.display="none";
+   var element = document.getElementById("hobbies").style.display="block";
+}
