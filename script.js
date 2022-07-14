1b545434482e13ad715b8ee7865744f8d37e113a
Ashish kumar anand <ashishraj5296@gmail.com>
2022-07-14 11:06:09 +0530

    Script for nav added

--- script.js
+++ script.js
@@ -10,6 +10,48 @@ function personal() {
    var element = document.getElementById("skills").style.display="none";
    var element = document.getElementById("education").style.display="none";
    var element = document.getElementById("hobbies").style.display="none";
+
+   var element = document.getElementById("listc").style.width="";
+   var element = document.getElementById("listc").style.height="";
+    var element = document.getElementById("listc").style.background="";
+    var element = document.getElementById("listc").style.paddingLeft="";
+    var element = document.getElementById("listc").style.borderRadius="";
+    var element = document.getElementById("listc").style.border="";
+    var element = document.getElementById("listc").style.color="";
+
+    var element = document.getElementById("lists").style.width="";
+    var element = document.getElementById("lists").style.height="";
+     var element = document.getElementById("lists").style.background="";
+     var element = document.getElementById("lists").style.paddingLeft="";
+     var element = document.getElementById("lists").style.borderRadius="";
+     var element = document.getElementById("lists").style.border="";
+     var element = document.getElementById("lists").style.color="";
+
+    var element = document.getElementById("listce").style.width="";
+    var element = document.getElementById("listce").style.height="";
+     var element = document.getElementById("listce").style.background="";
+     var element = document.getElementById("listce").style.paddingLeft="";
+     var element = document.getElementById("listce").style.borderRadius="";
+     var element = document.getElementById("listce").style.border="";
+     var element = document.getElementById("listce").style.color="";
+
+    var element = document.getElementById("listp").style.width="";
+    var element = document.getElementById("listp").style.height="";
+     var element = document.getElementById("listp").style.background="";
+     var element = document.getElementById("listp").style.paddingLeft="";
+     var element = document.getElementById("listp").style.borderRadius="";
+     var element = document.getElementById("listp").style.border="";
+     var element = document.getElementById("listp").style.color="";
+
+   var element = document.getElementById("list").style.width="180px";
+   var element = document.getElementById("list").style.height="40px";
+    var element = document.getElementById("list").style.background="linear-gradient(60deg, #f12711 30%, #f5af19 70%)";
+    var element = document.getElementById("list").style.paddingLeft="15px";
+    var element = document.getElementById("list").style.borderRadius="5%";
+    var element = document.getElementById("list").style.border="2px solid blue";
+    var element = document.getElementById("list").style.color="black";
+    
+
 }
 function contact() {
    var element = document.getElementById("personal").style.display="none";
@@ -19,6 +61,46 @@ function contact() {
    var element = document.getElementById("skills").style.display="none";
    var element = document.getElementById("education").style.display="none";
    var element = document.getElementById("hobbies").style.display="none";
+
+   var element = document.getElementById("list").style.width="";
+   var element = document.getElementById("list").style.height="";
+    var element = document.getElementById("list").style.background="";
+    var element = document.getElementById("list").style.paddingLeft="";
+    var element = document.getElementById("list").style.borderRadius="";
+    var element = document.getElementById("list").style.border="";
+    var element = document.getElementById("list").style.color="";
+
+    var element = document.getElementById("lists").style.width="";
+    var element = document.getElementById("lists").style.height="";
+     var element = document.getElementById("lists").style.background="";
+     var element = document.getElementById("lists").style.paddingLeft="";
+     var element = document.getElementById("lists").style.borderRadius="";
+     var element = document.getElementById("lists").style.border="";
+     var element = document.getElementById("lists").style.color="";
+
+    var element = document.getElementById("listce").style.width="";
+    var element = document.getElementById("listce").style.height="";
+     var element = document.getElementById("listce").style.background="";
+     var element = document.getElementById("listce").style.paddingLeft="";
+     var element = document.getElementById("listce").style.borderRadius="";
+     var element = document.getElementById("listce").style.border="";
+     var element = document.getElementById("listce").style.color="";
+
+    var element = document.getElementById("listp").style.width="";
+    var element = document.getElementById("listp").style.height="";
+     var element = document.getElementById("listp").style.background="";
+     var element = document.getElementById("listp").style.paddingLeft="";
+     var element = document.getElementById("listp").style.borderRadius="";
+     var element = document.getElementById("listp").style.border="";
+     var element = document.getElementById("listp").style.color="";
+
+    var element = document.getElementById("listc").style.width="180px";
+    var element = document.getElementById("listc").style.height="40px";
+     var element = document.getElementById("listc").style.background="linear-gradient(60deg, #f12711 30%, #f5af19 70%)";
+     var element = document.getElementById("listc").style.paddingLeft="15px";
+     var element = document.getElementById("listc").style.borderRadius="5%";
+     var element = document.getElementById("listc").style.border="2px solid blue";
+     var element = document.getElementById("listc").style.color="black";
 }
 function projects() {
    var element = document.getElementById("personal").style.display="none";
@@ -28,6 +110,49 @@ function projects() {
    var element = document.getElementById("skills").style.display="none";
    var element = document.getElementById("education").style.display="none";
    var element = document.getElementById("hobbies").style.display="none";
+
+   var element = document.getElementById("list").style.width="";
+   var element = document.getElementById("list").style.height="";
+    var element = document.getElementById("list").style.background="";
+    var element = document.getElementById("list").style.paddingLeft="";
+    var element = document.getElementById("list").style.borderRadius="";
+    var element = document.getElementById("list").style.border="";
+    var element = document.getElementById("list").style.color="";
+
+    var element = document.getElementById("lists").style.width="";
+    var element = document.getElementById("lists").style.height="";
+     var element = document.getElementById("lists").style.background="";
+     var element = document.getElementById("lists").style.paddingLeft="";
+     var element = document.getElementById("lists").style.borderRadius="";
+     var element = document.getElementById("lists").style.border="";
+     var element = document.getElementById("lists").style.color="";
+
+    
+    var element = document.getElementById("listc").style.width="";
+   var element = document.getElementById("listc").style.height="";
+    var element = document.getElementById("listc").style.background="";
+    var element = document.getElementById("listc").style.paddingLeft="";
+    var element = document.getElementById("listc").style.borderRadius="";
+    var element = document.getElementById("listc").style.border="";
+    var element = document.getElementById("listc").style.color="";
+
+    var element = document.getElementById("listce").style.width="";
+    var element = document.getElementById("listce").style.height="";
+     var element = document.getElementById("listce").style.background="";
+     var element = document.getElementById("listce").style.paddingLeft="";
+     var element = document.getElementById("listce").style.borderRadius="";
+     var element = document.getElementById("listce").style.border="";
+     var element = document.getElementById("listce").style.color="";
+
+    var element = document.getElementById("listp").style.width="180px";
+    var element = document.getElementById("listp").style.height="40px";
+     var element = document.getElementById("listp").style.background="linear-gradient(60deg, #f12711 30%, #f5af19 70%)";
+     var element = document.getElementById("listp").style.paddingLeft="15px";
+     var element = document.getElementById("listp").style.borderRadius="5%";
+     var element = document.getElementById("listp").style.border="2px solid blue";
+     var element = document.getElementById("listp").style.color="black";
+
+
 }
 function certificate() {
    var element = document.getElementById("personal").style.display="none";
@@ -37,6 +162,47 @@ function certificate() {
    var element = document.getElementById("skills").style.display="none";
    var element = document.getElementById("education").style.display="none";
    var element = document.getElementById("hobbies").style.display="none";
+
+   var element = document.getElementById("list").style.width="";
+   var element = document.getElementById("list").style.height="";
+    var element = document.getElementById("list").style.background="";
+    var element = document.getElementById("list").style.paddingLeft="";
+    var element = document.getElementById("list").style.borderRadius="";
+    var element = document.getElementById("list").style.border="";
+    var element = document.getElementById("list").style.color="";
+
+    
+    var element = document.getElementById("listc").style.width="";
+   var element = document.getElementById("listc").style.height="";
+    var element = document.getElementById("listc").style.background="";
+    var element = document.getElementById("listc").style.paddingLeft="";
+    var element = document.getElementById("listc").style.borderRadius="";
+    var element = document.getElementById("listc").style.border="";
+    var element = document.getElementById("listc").style.color="";
+
+    var element = document.getElementById("listp").style.width="";
+    var element = document.getElementById("listp").style.height="";
+     var element = document.getElementById("listp").style.background="";
+     var element = document.getElementById("listp").style.paddingLeft="";
+     var element = document.getElementById("listp").style.borderRadius="";
+     var element = document.getElementById("listp").style.border="";
+     var element = document.getElementById("listp").style.color="";
+
+     var element = document.getElementById("lists").style.width="";
+   var element = document.getElementById("lists").style.height="";
+    var element = document.getElementById("lists").style.background="";
+    var element = document.getElementById("lists").style.paddingLeft="";
+    var element = document.getElementById("lists").style.borderRadius="";
+    var element = document.getElementById("lists").style.border="";
+    var element = document.getElementById("lists").style.color="";
+
+     var element = document.getElementById("listce").style.width="180px";
+     var element = document.getElementById("listce").style.height="40px";
+      var element = document.getElementById("listce").style.background="linear-gradient(60deg, #f12711 30%, #f5af19 70%)";
+      var element = document.getElementById("listce").style.paddingLeft="15px";
+      var element = document.getElementById("listce").style.borderRadius="5%";
+      var element = document.getElementById("listce").style.border="2px solid blue";
+      var element = document.getElementById("listce").style.color="black";
 }
 function skills() {
    var element = document.getElementById("personal").style.display="none";
@@ -46,6 +212,46 @@ function skills() {
    var element = document.getElementById("skills").style.display="block";
    var element = document.getElementById("education").style.display="none";
    var element = document.getElementById("hobbies").style.display="none";
+
+   var element = document.getElementById("list").style.width="";
+   var element = document.getElementById("list").style.height="";
+    var element = document.getElementById("list").style.background="";
+    var element = document.getElementById("list").style.paddingLeft="";
+    var element = document.getElementById("list").style.borderRadius="";
+    var element = document.getElementById("list").style.border="";
+    var element = document.getElementById("list").style.color="";
+
+   var element = document.getElementById("listc").style.width="";
+   var element = document.getElementById("listc").style.height="";
+    var element = document.getElementById("listc").style.background="";
+    var element = document.getElementById("listc").style.paddingLeft="";
+    var element = document.getElementById("listc").style.borderRadius="";
+    var element = document.getElementById("listc").style.border="";
+    var element = document.getElementById("listc").style.color="";
+    
+    var element = document.getElementById("listce").style.width="";
+    var element = document.getElementById("listce").style.height="";
+     var element = document.getElementById("listce").style.background="";
+     var element = document.getElementById("listce").style.paddingLeft="";
+     var element = document.getElementById("listce").style.borderRadius="";
+     var element = document.getElementById("listce").style.border="";
+     var element = document.getElementById("listce").style.color="";
+
+    var element = document.getElementById("listp").style.width="";
+    var element = document.getElementById("listp").style.height="";
+     var element = document.getElementById("listp").style.background="";
+     var element = document.getElementById("listp").style.paddingLeft="";
+     var element = document.getElementById("listp").style.borderRadius="";
+     var element = document.getElementById("listp").style.border="";
+     var element = document.getElementById("listp").style.color="";
+
+     var element = document.getElementById("lists").style.width="180px";
+     var element = document.getElementById("lists").style.height="40px";
+      var element = document.getElementById("lists").style.background="linear-gradient(60deg, #f12711 30%, #f5af19 70%)";
+      var element = document.getElementById("lists").style.paddingLeft="15px";
+      var element = document.getElementById("lists").style.borderRadius="5%";
+      var element = document.getElementById("lists").style.border="2px solid blue";
+      var element = document.getElementById("lists").style.color="black";
 }
 function education() {
    var element = document.getElementById("personal").style.display="none";
